a29cf9b64fe648365b1596290a91523620f6a671
Sreeja <sreejapotu2000@gmail.com>
2023-07-27 11:53:38 -0400

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            Console.WriteLine("enter your first number ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter your second number ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = a + b;
+            Console.WriteLine("The sum is = " + c);
+            Console.ReadKey();
+            
+        }
+    }
+}
